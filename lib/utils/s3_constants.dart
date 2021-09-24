134b94ae2a93e2fd081eb4c59871d97a9d303500
TBraich <tribachhumg@gmail.com>
2021-09-24 22:07:08 +0700

    add comment in detail page

---
+++ lib/utils/s3_constants.dart
@@ -0,0 +1,6 @@
+import 'package:amazon_s3_cognito/aws_region.dart';
+
+const String BUCKET_NAME = "image-url-masterrecipe-bucket";
+const String IDENTITY_POOL = "ap-southeast-1_EyReYW6Vs";
+const String REGION = AwsRegion.AP_SOUTHEAST_1;
+const String SUB_REGION = AwsRegion.US_EAST_1;
